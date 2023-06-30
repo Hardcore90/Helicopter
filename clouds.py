a8b2d255ccc7974533b77b04e8c53573b355ddae
Dmitriy <maradonna05@yandex.ru>
2023-06-30 11:05:56 +0300

    Finished Урок№23

---
+++ clouds.py
@@ -0,0 +1,25 @@
+from utils import randboo
+
+class Clouds:
+    def __init__(self, w, h):
+       self.w = w
+       self.h = h
+       self.cells = [[0 for i in range(w)] for j in range(h)]
+
+    def update_clouds(self, r = 1, mxr = 20, g = 1, mxg = 10):
+        for i in range(self.h):
+            for j in range(self.w):
+                if randboo(r, mxr):
+                    self.cells[i][j] = 1
+                    if randboo(g, mxg):
+                        self.cells[i][j] = 2
+                else:
+                    self.cells[i][j] = 0
+
+    def export_data(self):
+        return {
+            "cells": self.cells
+        }
+
+    def import_data(self, data):
+        self.cells = data["cells"] or [[0 for i in range(self.w)] for j in range(self.h)]
